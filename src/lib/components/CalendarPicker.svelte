09dbb079630ad349a7850df45352922e577ab96e
sira313 <me@apoxi.cam>
2025-09-21 14:00:07 +0700

    fix duplicate input

--- src/lib/components/CalendarPicker.svelte
+++ src/lib/components/CalendarPicker.svelte
@@ -36,24 +36,3 @@
 	class="input input-bordered w-full"
 	class:input-disabled={disabled}
 />
-
-{#if label}
-	<label for={id} class="label">
-		<span class="label-text">{label}</span>
-		{#if required}
-			<span class="text-error">*</span>
-		{/if}
-	</label>
-{/if}
-
-<input 
-	type="date" 
-	{id}
-	{name}
-	{required}
-	{disabled}
-	bind:value
-	on:change={handleDateChange}
-	class="input input-bordered w-full"
-	class:input-disabled={disabled}
-/>
